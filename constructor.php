213d5dedcb3937ed38c0f60471df5feff62b08ed
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-09-25 10:40:02 +0700

    mengubah file constructor.php

--- constructor.php
+++ constructor.php
@@ -1,10 +1,18 @@
 <?php 
 //class
 	class Produk {
-		public $judul = "judul",
-			   $penulis = "penulis",
-			   $penerbit = "penerbit",
-			   $harga = 0;
+		public $judul,
+			   $penulis ,
+			   $penerbit,
+			   $harga;
+
+		//construtor
+		public function __construct($judul ="judul", $penulis ="penulis", $penerbit ="penerbit", $harga = 0){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+		}
 
 			   //sama seperti Ovaride
 		public function getLabel(){
@@ -12,23 +20,16 @@
 		}
 	}
 //instansiasi object
-	$produk3 = new Produk();
-	$produk3->judul ="Naruto";
-	$produk3->penulis =" Masaashi Kishimoto";
-	$produk3->penerbit = "Shonen Jump";
-	$produk3->harga = 30000;
+	$produk1 = new Produk("Naruto", "Masaashi Kishimoto", "Shonen", 300000);
+	$produk2 = new Produk("One Piece", "Ochiri Oda", "capCom", 250000);
+	$Produk3 = new produk("Call of Duty");
 
-	echo "Komik : $produk3->penulis, $produk3->penerbit ";
+	echo "Komik : $produk1->penulis, $produk1->penerbit ";
 	echo "<br>";
-
-	$produk4 = new Produk();
-	$produk4->judul ="One Piece";
-	$produk4->penulis =" Ochiri Oda";
-	$produk4->penerbit = "capCom";
-	$produk4->harga = 20000;
-
-	echo "Komik : ". $produk3->getLabel();
+	echo "Komik : ". $produk1->getLabel();
 	echo "<br>";
-	echo "Game : ". $produk4->getLabel();
+	echo "Game : ". $produk2->getLabel();
+	echo "<br>";
+	echo "Game :". $Produk3->getLabel();
 
  ?>
