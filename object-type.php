f7e13d4d7c74ad226cb29511e1f1329fbd15d4ed
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-09-25 10:43:04 +0700

    membuat file object-type.php

---
+++ object-type.php
@@ -0,0 +1,35 @@
+<?php 
+//class
+	class Produk {
+		public $judul,
+			   $penulis ,
+			   $penerbit,
+			   $harga;
+
+		//construtor
+		public function __construct($judul ="judul", $penulis ="penulis", $penerbit ="penerbit", $harga = 0){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+		}
+
+			   //sama seperti Ovaride
+		public function getLabel(){
+			return "$this->penulis, $this->penerbit";
+		}
+	}
+//instansiasi object
+	$produk1 = new Produk("Naruto", "Masaashi Kishimoto", "Shonen", 300000);
+	$produk2 = new Produk("One Piece", "Ochiri Oda", "capCom", 250000);
+	$Produk3 = new produk("Call of Duty");
+
+	echo "Komik : $produk1->penulis, $produk1->penerbit ";
+	echo "<br>";
+	echo "Komik : ". $produk1->getLabel();
+	echo "<br>";
+	echo "Game : ". $produk2->getLabel();
+	echo "<br>";
+	echo "Game :". $Produk3->getLabel();
+
+ ?>
