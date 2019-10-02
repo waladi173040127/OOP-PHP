883fa1863aeea055d4a1e7eb2ec990da03d6cda7
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-02 08:24:51 +0700

    latihan konsep abstract #14

---
+++ abstarct-class.php
@@ -0,0 +1,115 @@
+<?php 
+//class #14 -
+	abstract class Produk {
+		private $judul,
+			   $penulis ,
+			   $penerbit,
+			   $harga;
+
+		protected $diskon =0;
+		
+
+		//construtor
+		public function __construct($judul ="judul", $penulis ="penulis", $penerbit ="penerbit", $harga = 0){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+		
+		}
+		//Visibility
+		public function setHarga($harga){
+			$this->harga = $harga;
+		}
+		public function getHarga(){
+				return $this->harga - ($this->harga * $this->diskon /100);
+			}
+		//setter & getter
+		public function setJudul( $judul){
+			$this->judul = $judul;
+		}
+		public function getJudul(){
+			return $this->judul;
+		}
+		public function setPenulis($penulis){
+			$this->penulis = $penulis;
+		}
+		public function getPenulis(){
+			return $this->penulis;
+		}
+		public function setPenerbit($penerbit){
+			$this->penerbit= $penerbit;
+		}
+		public function getPenerbit(){
+			return $this->penerbit;
+		}
+			   //sama seperti Ovaride
+		public function getLabel(){
+			return "$this->penulis, $this->penerbit";
+		}
+		//method
+		abstract public function getInfoProduk();
+		public function getInfo(){
+			$str = "{$this->judul} | {$this->getLabel()} (RP. {$this->harga})";	
+			return $str;
+		}
+	}
+
+//kelas baru
+	//kelas komik
+	class Komik extends Produk{
+		public $jumHalaman;
+		public function __construct($judul ="judul", $penulis ="penulis", $penerbit ="penerbit", $harga = 0,  $jumHalaman = 0){
+			parent::__construct($judul, $penulis, $penerbit, $harga);
+			$this->jumHalaman = $jumHalaman;
+		}
+
+		public function getInfoProduk(){
+			//overriding
+			$str = "Komik :". $this->getInfo() ."- {$this->jumHalaman} Halaman.";
+			return $str;	
+		}
+	}
+	//kelas Game
+	class Game extends Produk{
+			public $waktuMain;
+			public function __construct($judul ="judul", $penulis ="penulis", $penerbit ="penerbit", $harga = 0,  $waktuMain = 0){
+				parent:: __construct($judul, $penulis, $penerbit, $harga);
+				$this->waktuMain=$waktuMain;
+			}
+			//Visibility
+			public function setDiskon($diskon){
+				$this->diskon = $diskon;
+			}
+			public function getInfoProduk(){
+				//overriding
+			$str = "Game : ". $this->getInfo() ." ~ {$this->waktuMain} Jam.";
+			return $str;
+		}
+	}
+	//kelas cetakInfoProduk
+	class CetakInfoProduk{
+		public $daftarProduk = array();
+
+		public function tambahProduk( Produk $produk){
+			$this->daftarProduk[] = $produk;
+		}
+		public function cetak( ){
+			$str = "DAFTAR PRODUK : <br>";
+			foreach($this->daftarProduk as $p){
+				$str .= "- {$p->getInfoProduk()} <br>";
+			}
+			return $str;
+		}
+	}
+//instansiasi object
+	$produk1 = new Komik("Naruto", "Masaashi Kishimoto", "Shonen", 300000, 100);
+	$produk2 = new Game("One Piece", "Ochiri Oda", "capCom", 250000,50);
+	
+	$cetakProduk = new CetakInfoProduk();
+	$cetakProduk->tambahProduk( $produk1);
+	$cetakProduk->tambahProduk( $produk2);
+	echo $cetakProduk->cetak();
+	
+
+ ?>
